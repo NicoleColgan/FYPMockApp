d43b55a5652be28a68e84a121666803be4fd30cf
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-01-29 11:46:01 +0000

    connecting to db

--- ibs-system-backend/src/main/java/com/ibs/backend/controller/UserController.java
+++ ibs-system-backend/src/main/java/com/ibs/backend/controller/UserController.java
@@ -4,11 +4,9 @@ package com.ibs.backend.controller;
 import com.ibs.backend.model.User;
 import com.ibs.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
